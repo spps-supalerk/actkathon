912d53b6c6de9ec25f60337960016c84905d3f23
Supalerk Pisitsupakarn <spps@Supalerks-MacBook-Air.local>
2021-08-12 09:40:57 +0700

    Update clean comp_name

---
+++ act_func.py
@@ -0,0 +1,15 @@
+# this project won't use company name
+def clean_comp(x, remove_lst):
+  x = str(x)
+  for t in remove_lst:
+    if t in x:
+      x = x.replace(t,'')
+  x = x.strip()
+  return x
+start_lst = ['บริษัท', 'ห้างหุ้นส่วนจำกัด', 'บริษัทจำกัด']
+end_lst = ['จำกัด(มหาชน)','จำกัด(สัญญากิจการค้าร่วม)', 'จำกัด', '(มหาชน)']
+punc = [' ','(', ')','.']
+remove_lst = start_lst + end_lst + punc
+
+df_sub['comp_name_clean'] = df_sub['comp_name'].apply(clean_comp, remove_lst=remove_lst)
+df_win['comp_name_clean'] = df_win['comp_name'].apply(clean_comp, remove_lst=remove_lst)
