170612bf18c7d622092970a7386cfb3469a3b170
Supalerk Pisitsupakarn <spps@Supalerks-MacBook-Air.local>
2021-08-12 10:03:55 +0700

    add doc string to clean comp_name

--- act_func.py
+++ act_func.py
@@ -1,15 +1,27 @@
-# this project won't use company name
-def clean_comp(x, remove_lst):
-  x = str(x)
-  for t in remove_lst:
-    if t in x:
-      x = x.replace(t,'')
-  x = x.strip()
-  return x
-start_lst = ['บริษัท', 'ห้างหุ้นส่วนจำกัด', 'บริษัทจำกัด']
-end_lst = ['จำกัด(มหาชน)','จำกัด(สัญญากิจการค้าร่วม)', 'จำกัด', '(มหาชน)']
-punc = [' ','(', ')','.', '"', "'"]
-remove_lst = start_lst + end_lst + punc
+def clean_comp(comp_name, remove_lst):
+  '''
+  Returns clean company name which will be use for mapping with other sources
 
-# df_sub['comp_name_clean'] = df_sub['comp_name'].apply(clean_comp, remove_lst=remove_lst)
-# df_win['comp_name_clean'] = df_win['comp_name'].apply(clean_comp, remove_lst=remove_lst)
+    Parameters:
+      comp_name (str): A company name 
+      remove_lst (list): list of keyword which needs to replace with empty string
+
+    Returns:
+      clean_company (str): company name after cleaned
+
+    Example:
+      start_lst = ['บริษัท', 'ห้างหุ้นส่วนจำกัด', 'บริษัทจำกัด']
+      end_lst = ['จำกัด(มหาชน)','จำกัด(สัญญากิจการค้าร่วม)', 'จำกัด', '(มหาชน)']
+      punc = [' ','(', ')','.', '"', "'"]
+      remove_lst = start_lst + end_lst + punc
+
+      df_sub['comp_name_clean'] = df_sub['comp_name'].apply(clean_comp, remove_lst=remove_lst)
+      df_win['comp_name_clean'] = df_win['comp_name'].apply(clean_comp, remove_lst=remove_lst)
+  '''
+
+  comp_name = str(comp_name)
+  for keyword in remove_lst:
+    if keyword in comp_name:
+      comp_name = comp_name.replace(keyword,'')
+  comp_name = comp_name.strip()
+  return comp_name
