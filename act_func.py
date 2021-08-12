bc8e5e212c4ba8443bf1605dad3f96d2fa68f4bb
Supalerk Pisitsupakarn <spps@Supalerks-MacBook-Air.local>
2021-08-12 09:52:55 +0700

    update func clean_comp

--- act_func.py
+++ act_func.py
@@ -8,8 +8,8 @@ def clean_comp(x, remove_lst):
   return x
 start_lst = ['บริษัท', 'ห้างหุ้นส่วนจำกัด', 'บริษัทจำกัด']
 end_lst = ['จำกัด(มหาชน)','จำกัด(สัญญากิจการค้าร่วม)', 'จำกัด', '(มหาชน)']
-punc = [' ','(', ')','.']
+punc = [' ','(', ')','.', '"', "'"]
 remove_lst = start_lst + end_lst + punc
 
-df_sub['comp_name_clean'] = df_sub['comp_name'].apply(clean_comp, remove_lst=remove_lst)
-df_win['comp_name_clean'] = df_win['comp_name'].apply(clean_comp, remove_lst=remove_lst)
+# df_sub['comp_name_clean'] = df_sub['comp_name'].apply(clean_comp, remove_lst=remove_lst)
+# df_win['comp_name_clean'] = df_win['comp_name'].apply(clean_comp, remove_lst=remove_lst)
